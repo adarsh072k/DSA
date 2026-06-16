c5028e73d704d32e9b5c1a4bd970a81ef49bdf84
Adarsh Prakash <adarshnobilian@gmail.com>
2026-06-17 03:36:50 +0530

    Added problem5

--- problem5.java
+++ problem5.java
@@ -1,1 +1,17 @@
 /* */
+
+class Solution
+{
+    public static void accessCharacters(String str)
+    {
+        for(int i=0;i<str.length();i++)
+        {
+            System.out.println(str.charAt(i));
+        }
+    }
+    public static void main(String[] args)
+    {
+        String word = "adarsh";
+        accessCharacters(word);
+    }
+}

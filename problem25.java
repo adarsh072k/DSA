1e67db6f01b671affe8c260d29aa02926732bf91
Adarsh Prakash <adarshnobilian@gmail.com>
2026-07-04 05:34:48 +0530

    added pattern problems

---
+++ problem25.java
@@ -0,0 +1,33 @@
+/*
+ */
+
+public class problem25 {
+    public static void pattern12(int n) 
+    {
+        for(int i=1;i<=n;i++)
+        {
+            int middleSpaces = 2 * (n - i) - 1;
+            //left pattern
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            //middle space
+            for(int k=0;k<=middleSpaces;k++)
+            {
+                System.out.print(" ");
+            }
+            //right pattern
+            for(int jj=i;jj>=1;jj--)
+            {
+                System.out.print(jj);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        int nums=4;
+        pattern12(nums);
+    }
+}
